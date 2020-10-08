301d44189f11c91ad496bb7e992993c088260a1e
John Jaramillo <jjaramillo1221@gmail.com>
2020-10-07 21:58:32 -0600

    Implemented unit testing for Palindromes.testRecursive.

--- src/edu/cnm/deepdive/Palindromes.java
+++ src/edu/cnm/deepdive/Palindromes.java
@@ -4,8 +4,7 @@ public class Palindromes {
 
   public static boolean testRecursive(String phrase) {
     return (phrase.length() <= 1)
-        || (
-        phrase.charAt(0) == phrase.charAt(phrase.length() - 1)
+        || (phrase.charAt(0) == phrase.charAt(phrase.length() - 1)
             && testRecursive(phrase.substring(1, phrase.length() - 1))
     );
   }
